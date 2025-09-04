7acf1561883a885a644dfb2ede7da71e3ae6ab4e
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2025-09-04 20:19:40 -0300

    merge develop e usuario

--- obj/Debug/net7.0/backend.AssemblyInfo.cs
+++ obj/Debug/net7.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99cf25f2329fa31fb9aad1775bedeaf9d21cdfbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+814ea49796d28291d4557704b293f2507bf630a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
