7a4ecb0072e09e8a6ee6b975bbb4c14686be4d78
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2025-09-04 21:30:07 -0300

    teste

--- obj/Debug/net7.0/backend.AssemblyInfo.cs
+++ obj/Debug/net7.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caad4d564a39ee818e699b73a382eac7289a0611")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28819f92c4f204852d83d10b88ef2d71272f7f3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
