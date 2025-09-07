e995e61f23a8e5620451cd1488853ced2d1784c6
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2025-09-07 12:52:31 -0300

    feat: inserindo novamente pasta Migrations

---
+++ backend/Migrations/20250907155140_DadosDasTabelas.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class DadosDasTabelas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Usuarios",
+                keyColumn: "IdUsuario",
+                keyValue: 1,
+                column: "DateNow",
+                value: new DateTime(2025, 9, 7, 15, 51, 39, 917, DateTimeKind.Utc).AddTicks(9195));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Usuarios",
+                keyColumn: "IdUsuario",
+                keyValue: 1,
+                column: "DateNow",
+                value: new DateTime(2025, 9, 7, 13, 39, 35, 670, DateTimeKind.Utc).AddTicks(6288));
+        }
+    }
+}
